2a6f18de228a371bf1ee68d67d0fe7397cca3fb2
up201905810 <carolinavferreira>
2022-04-21 16:32:01 +0100

    inicio

--- site.js
+++ site.js
@@ -11,5 +11,11 @@ function showCategories() {
 }
 
 document.getElementById("criar").addEventListener("click", function(){
+  document.getElementById("container").style.display = "none";
   document.getElementById("criarconta").style.display = "block";
+});
+
+document.getElementById("iniciar_sessao").addEventListener("click", function(){
+  document.getElementById("container").style.display = "block";
+  document.getElementById("criarconta").style.display = "none";
 });
